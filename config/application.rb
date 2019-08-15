b9da014d66725090932b8421fbeaf238a5ac7604
kurogoro <kurogoro0241@gmail.com>
2019-08-16 00:05:59 +0900

    Set i18n-local ja

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module TidyTasks
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.default_locale = :ja
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
