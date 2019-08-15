bec06435288fc11d68c926934a78aeff00fee179
kurogoro <kurogoro0241@gmail.com>
2019-08-16 00:11:03 +0900

    Set helper to change class names whether day is today

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def set_class_today(day, today, class_array)
+    if day == today
+      class_array << "main-page__body__table__inner__column__row--date--today"
+    end
+    return class_array
+  end
+
+  def set_class_today_top(day, today, class_array)
+    if day == today
+      class_array << ["main-page__body__table__inner__column__row--date--today", "main-page__body__table__inner__column__row--border-top-red"]
+    end
+    return class_array
+  end
 end
