9b3b396c38d0a299a70d46a4af9af0a0dafc8d32
kurogoro <kurogoro0241@gmail.com>
2019-08-25 20:21:52 +0900

    Modify project, work_process and task tables

--- db/migrate/20190810000916_create_projects.rb
+++ db/migrate/20190810000916_create_projects.rb
@@ -1,9 +1,10 @@
 class CreateProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
-      t.string :name
+      t.string :name, null: false, unique: true
       t.text :detail
       t.datetime :deadline
+      t.integer :public_access_id, null: false
       t.timestamps
     end
   end
--- db/migrate/20190810001718_create_work_processes.rb
+++ db/migrate/20190810001718_create_work_processes.rb
@@ -1,7 +1,7 @@
 class CreateWorkProcesses < ActiveRecord::Migration[5.2]
   def change
     create_table :work_processes do |t|
-      t.string :name
+      t.string :name, null: false, unique: true
       t.text :detail
       t.timestamps
     end
