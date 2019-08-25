ab7185f13f873c9ca7fabab9b2e9b1c68520be4d
kurogoro <kurogoro0241@gmail.com>
2019-08-25 20:27:35 +0900

    Create groups and people controllers routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :people
   root 'tasks#index'
   resources :tasks, only: [:index]
+  resources :groups, only: [:new, :create]
+  resources :people, only: [:index] do
+    collection do
+      get 'search'
+    end
+  end
 end
