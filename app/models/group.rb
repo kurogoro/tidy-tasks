743684a85da66006a318eb1da5d67b181ae16264
kurogoro <kurogoro0241@gmail.com>
2019-08-26 11:56:29 +0900

    Modify group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,6 @@ class Group < ApplicationRecord
   belongs_to :administrator, class_name: "Person", foreign_key: 'administrator_id'
   has_many :group_members, dependent: :delete_all
   has_many :members, class_name: "Person", through: :group_members, source: :member
-  has_many :project_groups
+  has_many :project_groups, dependent: :delete_all
   has_many :projects, through: :project_groups
 end
