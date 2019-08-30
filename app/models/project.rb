9bfbef110e7a2dcb973e5d0f74a8e146bb651f28
kurogoro <kurogoro0241@gmail.com>
2019-08-30 22:19:05 +0900

    Modify person and project models

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,8 @@ class Project < ApplicationRecord
   belongs_to :administrator, class_name: "Person", foreign_key: 'administrator_id'
   has_many :project_groups, dependent: :delete_all
   has_many :groups, through: :project_groups
+  has_many :project_members, dependent: :delete_all
+  has_many :members, class_name: "Person", through: :project_members, source: :member
   has_many :tasks
   belongs_to_active_hash :public_access
 end
